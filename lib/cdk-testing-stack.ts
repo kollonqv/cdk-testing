b894ca5bb38feb8995e19d1f91817f527afe94b7
Kalle Ollonqvist <ollonqvistkalle@gmail.com>
2022-07-14 07:25:43 -0700

    Add lambda

--- lib/cdk-testing-stack.ts
+++ lib/cdk-testing-stack.ts
@@ -2,16 +2,21 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as cdk from "aws-cdk-lib";
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class CdkTestingStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
+    // create sqs queue
     const queue = new sqs.Queue(this, 'CdkTestingQueue', {
       visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
+    new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.PYTHON_3_7,
+      handler: 'app.lambda_handler',
+      code: lambda.Code.fromInline('./lambdas')
     });
   }
 }
--- test/cdk-testing.test.ts
+++ test/cdk-testing.test.ts
@@ -2,8 +2,6 @@ import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as CdkTesting from '../lib/cdk-testing-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-testing-stack.ts
 test('SQS Queue Created', () => {
   const app = new cdk.App();
     // WHEN
